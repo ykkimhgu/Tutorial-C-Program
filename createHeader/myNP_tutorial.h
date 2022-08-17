7422e0c68550218b7767677a1556b8068413051d
ykkimhgu <38373000+ykkimhgu@users.noreply.github.com>
2022-08-17 17:11:46 +0900

    updated

---
+++ createHeader/myNP_tutorial.h
@@ -0,0 +1,22 @@
+/*----------------------------------------------------------------\
+@ Numerical Methods by Young-Keun Kim - Handong Global University
+
+Author           : YOUR NAME
+Created          : 05-03-2021
+Modified         : 05-03-2021
+Language/ver     : C in MSVS2019
+
+Description      : myNM.h
+/----------------------------------------------------------------*/
+
+#ifndef		_MY_NM_H		// use either (#pragma once) or  (#ifndef ...#endif)
+#define		_MY_NM_H
+#define		PI		3.14159265358979323846264338327950288419716939937510582
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+extern double factorial(double _x);
+
+#endif
