7907bb693b23b61dc559b3c3ce80ab90f2cca386
Yjinsu <84503980+Yjinsu@users.noreply.github.com>
2022-08-19 01:45:00 +0900

    add description

--- installVisualStudio/C_visualStudio_exercise2.c
+++ installVisualStudio/C_visualStudio_exercise2.c
@@ -1,3 +1,12 @@
+/*-------------------------------------------------------------------------------\
+@ C Programming Tutorial by Young-Keun Kim - Handong Global University
+Author           : SSS LAB
+Created          : 19-08-2022
+Modified         : 19-08-2022
+Language/ver     : C in MSVS2022
+Description      : C_visualStudio_exercise2.c
+-------------------------------------------------------------------------------*/
+
 #include <stdio.h>
 
 int main() {
