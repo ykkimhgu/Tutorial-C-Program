7a17cf1c1260c7bd0d6e4186cbe090bfed0964bd
ykkimhgu <38373000+ykkimhgu@users.noreply.github.com>
2022-08-28 15:28:55 +0900

    Update myNM_tutorial.c

--- sineTaylor/myNM_tutorial.c
+++ sineTaylor/myNM_tutorial.c
@@ -19,3 +19,36 @@ double factorial(double _x)
 		return _x * factorial(_x - 1);
 }
 
+/*
+//  Taylor series approximation for sin(x) using pre-defined functions (input unit: [rad])
+double sinTaylor(double _x) 
+{
+	int N_max = 20;
+	double epsilon = 1e-5;
+
+	double S_N = 0, S_N_prev = 0, rel_chg = 0;
+	int N = 0;
+
+	do {
+		// [TODO] YOUR CODE GOES HERE
+		// [TODO] YOUR CODE GOES HERE
+		// [TODO] YOUR CODE GOES HERE
+		// [TODO] YOUR CODE GOES HERE
+		
+	} while (N < N_max && rel_chg >= epsilon);
+
+	return S_N;
+}
+
+// Taylor series approximation for sin(x) using pre-defined functions (input unit: [deg])
+double sindTaylor(double _x) 
+{
+	// [TODO] YOUR CODE GOES HERE
+}
+
+// Taylor series approximation for sin(x) without using pre-defined functions (input unit: [rad])
+double sinTaylor2(double _x) 
+{
+	// [TODO] YOUR CODE GOES HERE
+}
+*/
