d6c8e6ad64ebdb4fcf541af974642e5adb5d9f4e
ykkimhgu <38373000+ykkimhgu@users.noreply.github.com>
2022-09-01 22:01:19 +0900

    Update C_taylorSeries_exercise_part2.c

--- sineTaylor/C_taylorSeries_exercise_part2.c
+++ sineTaylor/C_taylorSeries_exercise_part2.c
@@ -27,8 +27,7 @@ int main(int argc, char* argv[])
 	printf("\n\n");
 	printf("=======================================\n");
 	printf("    sin( %f[rad] ) Calculation   \n", x);
-	printf("=======================================\n");
-	printf("   -  iteration No. = %d        \n", N);
+	printf("=======================================\n");	
 	printf("   -  My     result = %3.12f    \n", S_N);
 	printf("   -  Math.h result = %3.12f    \n", sin(x));
 	printf("   -  absolute err. = %3.12f    \n", S_N - sin(x));
@@ -39,33 +38,3 @@ int main(int argc, char* argv[])
 }
 
 
-
-//  Taylor series approximation for sin(x) using pre-defined functions (input unit: [rad])
-double sinTaylor(double _x)
-{
-	int N_max = 20;
-	double epsilon = 1e-5;
-
-	double S_N = 0, S_N_prev = 0, rel_chg = 0;
-	int N = 0;
-
-	do {
-		N++;
-		S_N_prev = S_N;
-		S_N = 0;
-		// [TODO] add your algorithm here
-		// [TODO] add your algorithm here
-		// [TODO] add your algorithm here
-
-	} while (N < N_max && rel_chg >= epsilon);
-
-	return S_N;
-}
-
-/*
-// Taylor series approximation for sin(x) using pre-defined functions (input unit: [deg])
-double sindTaylor(double _x)
-{
-	// [TODO] add your algorithm here
-}
-*/
