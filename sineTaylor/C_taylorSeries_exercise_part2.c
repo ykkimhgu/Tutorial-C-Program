a948e3a065ab36b221da9fd63fa2f756fbe1f5e9
ykkimhgu <38373000+ykkimhgu@users.noreply.github.com>
2022-09-01 21:15:18 +0900

    Update C_taylorSeries_exercise_part2.c

--- sineTaylor/C_taylorSeries_exercise_part2.c
+++ sineTaylor/C_taylorSeries_exercise_part2.c
@@ -1,1 +1,71 @@
+/*----------------------------------------------------------------\
+@ Numerical Methods by Young-Keun Kim - Handong Global University
+Author           : YOUR NAME
+Created          : 05-03-2021
+Modified         : 05-03-2021
+Language/ver     : C in MSVS2019
+Description      : myNM_main.c
+----------------------------------------------------------------*/
 
+#include "myNM_tutorial.h"
+
+int main(int argc, char* argv[])
+{
+
+	double x = PI / 3;
+	
+	int N_max = 20;
+	double epsilon = 1e-5;
+
+	double S_N = 0, S_N_prev = 0, rel_chg = 0;
+	int N = 0;
+
+	/*===== Select the function to call =====*/
+	S_N = sinTaylor(x);
+	
+	
+	printf("\n\n");
+	printf("=======================================\n");
+	printf("    sin( %f[rad] ) Calculation   \n", x);
+	printf("=======================================\n");
+	printf("   -  iteration No. = %d        \n", N);
+	printf("   -  My     result = %3.12f    \n", S_N);
+	printf("   -  Math.h result = %3.12f    \n", sin(x));
+	printf("   -  absolute err. = %3.12f    \n", S_N - sin(x));
+	printf("=======================================\n");
+	
+	system("pause");
+	return 0;
+}
+
+
+
+//  Taylor series approximation for sin(x) using pre-defined functions (input unit: [rad])
+double sinTaylor(double _x)
+{
+	int N_max = 20;
+	double epsilon = 1e-5;
+
+	double S_N = 0, S_N_prev = 0, rel_chg = 0;
+	int N = 0;
+
+	do {
+		N++;
+		S_N_prev = S_N;
+		S_N = 0;
+		// [TODO] add your algorithm here
+		// [TODO] add your algorithm here
+		// [TODO] add your algorithm here
+
+	} while (N < N_max && rel_chg >= epsilon);
+
+	return S_N;
+}
+
+/*
+// Taylor series approximation for sin(x) using pre-defined functions (input unit: [deg])
+double sindTaylor(double _x)
+{
+	// [TODO] add your algorithm here
+}
+*/
