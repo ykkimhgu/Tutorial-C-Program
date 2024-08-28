175130c208b4f10050e491321cecd2c38745c4cc
ykkimhgu <38373000+ykkimhgu@users.noreply.github.com>
2024-08-28 12:05:01 +0900

    Update C_structure_exercise2_solution.c

--- structure/solution/C_structure_exercise2_solution.c
+++ structure/solution/C_structure_exercise2_solution.c
@@ -7,11 +7,13 @@ Language/ver     : C in MSVS2022
 Description      : C_structure_exercise2_solution.c
 -------------------------------------------------------------------------------*/
 
-#define _CRT_SECURE_NO_WARNINGS
+
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#define _CRT_SECURE_NO_WARNINGS
 
 typedef struct {
 	char building_name[100];
@@ -22,20 +24,20 @@ typedef struct {
 
 int main()
 {
-    // Create structure type (Handong)  variable  room1
+   // Create structure type (Handong)  variable  room1
     Handong room1;
+
     // Create structure type (Handong)  variable  room2, room3
     Handong room2;
     Handong room3;
-    
     // Create structure type (Handong)  Pointer , roomPt
-    Handong *roomPt= malloc(sizeof(Handong));
-    
-    // Assign room3 address to roomPt
-    roomPt=&room3;
+    Handong *room1Pt = &room1;
 
-  
-    // Define structure variable  memeber values: room1
+    // Assign room2 and room3 address to roomPts
+	Handong *room2Pt = &room2;
+	Handong *room3Pt = &room3;
+
+    // Define structure variable  member values: room1
 	strcpy(room1.building_name, "Newton");
 	room1.room_number = 109;
 	strcpy(room1.room_name, "SSSLAB");
@@ -45,24 +47,26 @@ int main()
 	room2.room_number = 118;
 	strcpy(room2.room_name, "Control-Lab");
 
+
     // Define structure variable  memeber values: room3
 	strcpy(room3.building_name, "Newton");
 	room3.room_number = 119;
 	strcpy(room3.room_name, "SW-Lab");
 
-    
+
     // Print each member values  : room1, room2, room3
 	printf("%s building, room  %d  is %s\n", room1.building_name, room1.room_number, room1.room_name);
 	printf("%s building, room  %d  is %s\n", room2.building_name, room2.room_number, room2.room_name);
 	printf("%s building, room  %d  is %s\n", room3.building_name, room3.room_number, room3.room_name);
-	
+
 	// Print each member values by  pointer variable: roomPt
-	printf("%s building, room  %d  is %s\n", roomPt->building_name, roomPt->room_number, roomPt->room_name);
-	
+	printf("%s building, room  %d  is %s\n", room1Pt->building_name, room1Pt->room_number, room1Pt->room_name);
+	printf("%s building, room  %d  is %s\n", room2Pt->building_name, room2Pt->room_number, room2Pt->room_name);
+	printf("%s building, room  %d  is %s\n", room3Pt->building_name, room3Pt->room_number, room3Pt->room_name);
+
 	// Print address of   room3  and value of roomPt and compare. 
-	printf("\n room3 address=%x ,  roomPt = %x  \n", &room3, roomPt);
-	
-	// Free mallocated variable 
-	//free(roomPt);
+	printf("\n room1 address=%x ,  roomPt = %x  \n", &room1, room1Pt);
+	printf("\n room2 address=%x ,  roomPt = %x  \n", &room2, room2Pt);
+	printf("\n room3 address=%x ,  roomPt = %x  \n", &room3, room3Pt);
 
 }
