47bb9a986a8614ba1eb4fb8d5020b65372d68421
ykkimhgu <38373000+ykkimhgu@users.noreply.github.com>
2024-08-30 17:59:03 +0900

    Update C_pointer_exercise2_solution.c

--- pointer-array/solution/C_pointer_exercise2_solution.c
+++ pointer-array/solution/C_pointer_exercise2_solution.c
@@ -13,18 +13,22 @@ int main()
 {
 	int x = 10;
 	double y = 2.5;
+	
 	int* ptrX = &x;
-	int* ptrY = &y;
-	
-	printf("Print the address of variable ¡®x¡¯= %d\n", &x);
-	printf("Print the address of variable ¡®y¡¯= %d\n", &y);
-	printf("Print the value of pointer ¡®ptrX¡¯= %d\n", ptrX);
-	printf("Print the address of pointer ¡®ptrX¡¯= %d\n", &ptrX);
-	printf("Print the size of pointer ¡®ptrX' = %d\n\n\n", sizeof(ptrX));
-	
+	double* ptrY = &y;
 
-	printf("Print the value of pointer ¡®ptrY¡¯= %d\n", ptrY);
-	printf("Print the address of pointer ¡®ptrY¡¯= %d\n", &ptrY);
-	printf("Print the size of pointer ¡®ptrY' = %d\n\n\n", sizeof(ptrY));
-	
+	printf("Print the address of variable x= %x\n", &x);
+	printf("Print the value pointed by ptrX = %d\n", *ptrX);
+	printf("Print the value of pointer ptrX= %x\n", ptrX);
+	printf("Print the address of pointer ptrX= %x\n", &ptrX);
+	printf("Print the size of x	 = %d\n", sizeof(x));
+	printf("Print the size of pointer ptrX = %d\n\n\n", sizeof(ptrX));
+
+	printf("Print the address of variable y= %x\n", &y);
+	printf("Print the value pointed by ptrY = %f\n", *ptrY);
+	printf("Print the value of pointer ptrY= %x\n", ptrY);
+	printf("Print the address of pointer ptrY= %x\n", &ptrY);
+	printf("Print the size of y	  = %d\n", sizeof(y));
+	printf("Print the size of pointer ptrY = %d\n\n\n", sizeof(ptrY));
+
 }
